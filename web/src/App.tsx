f3230a410bf473daa764220c7be49cbe09cbfaa9
Antonio Neto <nettoveloso@gmail.com>
2020-10-18 15:30:43 -0300

    Add Project Web

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn';
+//import SignUp from './pages/SignUp';
+import GlobalStyles from './styles/global';
+
+const App : React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyles />
+  </>
+);
+
+export default App;

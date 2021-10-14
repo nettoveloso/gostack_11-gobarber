646891f11ddc856f5203fe69fde48b7ed76e7cf0
nettoveloso@gmail.com <nettoveloso@MacBook-Pro-de-Neto.local>
2021-10-14 00:30:01 -0300

    Add Provider List

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import React, { useCallback }from 'react';
+import React, { useCallback, useState, useEffect }from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
@@ -8,18 +10,45 @@ import {
   HeaderTitle,
   Username,
   ProfileButton,
-  UserAvatar
+  UserAvatar,
+  ProvidersList,
+  ProvideContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText,
+  ProvidersListTitle
 } from './styles';
+import api from '../../services/api';
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
-  const { singOut, user } = useAuth();
+  const [providers, setProviders] = useState<Provider[]>([]);
 
+  const { singOut, user } = useAuth();
   const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('providers').then( (response) => {
+      setProviders(response.data);
+    });
+  }, []);
 
   const navigateToProfile = useCallback(() => {
       navigate('Profile');
     },[navigate],
   );
+
+  const navigateToCreateAppointment = useCallback((providerId: string) => {
+    navigate('CreateAppointment', { providerId });
+  },[navigate],
+);
 
   return (
     <Container>
@@ -36,6 +65,36 @@ const Dashboard: React.FC = () => {
         </ProfileButton>
 
       </Header>
+
+      <ProvidersList
+        data={providers}
+        ListHeaderComponent={
+          <ProvidersListTitle>Barbeiros</ProvidersListTitle>
+        }
+        keyExtractor={(provider) => provider.id}
+        renderItem={({item : provider }) => (
+
+          <ProvideContainer onPress={()=> navigateToCreateAppointment(provider.id)}>
+            <ProviderAvatar source={{ uri: provider.avatar_url }} />
+            <ProviderInfo>
+              <ProviderName>{ provider.name }</ProviderName>
+
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+                <ProviderMetaText>Segunda à Sexta</ProviderMetaText>
+              </ProviderMeta>
+
+              <ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+                <ProviderMetaText>8h às 18h</ProviderMetaText>
+              </ProviderMeta>
+
+            </ProviderInfo>
+          </ProvideContainer>
+
+        )}
+      />
+
     </Container>
   );
 };
